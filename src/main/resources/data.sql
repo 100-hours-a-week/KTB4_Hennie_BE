a6157f8a1f17b6ad53f9199e295112ecc969b236
iey704 <iey704@gachon.ac.kr>
2026-07-26 22:13:17 +0900

    chore: 개발 환경 불필요 파일 정리

--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO users (email, password, nickname, profile_url, author_deleted, created_at, modified_at)
-VALUES ('tester@adapterz.kr', '123aS!', 'Adapterz', 'imgUrl', false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);

660cd4c7387f986c71e2720bfba4879086e27d0a
iey704 <iey704@gachon.ac.kr>
2026-08-09 00:56:34 +0900

    feat: 대댓 글 글자 수 제한 추가

--- src/main/java/com/hennie/springdatajpa/domain/comment/dto/request/ReplyCreateRequestDto.java
+++ src/main/java/com/hennie/springdatajpa/domain/comment/dto/request/ReplyCreateRequestDto.java
@@ -1,6 +1,8 @@
 package com.hennie.springdatajpa.domain.comment.dto.request;
 
+import com.hennie.springdatajpa.domain.comment.entity.Comment;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +11,7 @@ import lombok.NoArgsConstructor;
 public class ReplyCreateRequestDto {
 
     @NotBlank(message = "대댓글 내용은 필수값입니다.")
+    @Size(max = Comment.CONTENT_MAX_LENGTH, message = "대댓글은 3000자 이하여야 합니다.")
     private String content;
 
     private Long replyToId;
